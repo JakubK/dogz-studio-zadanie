2501a2ec740463a4f6d2813cbe5319e5b7279732
JakubK <jakubkuniszewski1@gmail.com>
2020-07-15 12:29:30 +0200

    Add Form component with ability to send post request

---
+++ src/components/Form/Form.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit="handleSubmit" class="form">
+    <k-input v-model="email" :label="'Email'"/>
+    <k-input v-model="password" type="password" :label="'Password'"/>
+    <kendo-button class="k-primary">Submit</kendo-button>
+  </form>
+</template>
+<script>
+export default {
+  name: 'Form',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async handleSubmit(e) {
+      e.preventDefault()
+      let response = await fetch('https://dogz.studio/api/login', {
+        method: 'POST',
+        mode: 'cors', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: this.email,
+          password: this.password
+        }) 
+    }).then(res => res.json())
+    }
+  }
+  
+}
+</script>
+<style>
+  .form {
+    align-items: center;
+    border-radius: 4px;
+    background: #FEFEFE;
+    box-shadow: 0px 0px 8px rgba(32, 37, 37, 0.15);
+  }
+
+  .form > * 
+  {
+    margin: 10px;
+  }
+</style>
